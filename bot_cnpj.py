07c4eea3486f2bd828b94a80fcd8047c56a2022a
RafaelMPeixoto <rafael_deicide@hotmail.com>
2022-05-09 13:17:26 -0300

    Commit listagem site

--- bot_cnpj.py
+++ bot_cnpj.py
@@ -1,11 +1,13 @@
+from xml.dom.minidom import Document
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
 
 
 class Bot:
     def run(playwright):
         # cnpj = ['02.290.482/0001-97']
         firefox = playwright.firefox
-        browser = firefox.launch(headless=False)
+        browser = firefox.launch(headless=False, slow_mo=50)
         page = browser.new_page()
         page.goto("https://servicos.receita.fazenda.gov.br/servicos/cnpjreva/Cnpjreva_Solicitacao.asp")
 
@@ -15,9 +17,9 @@ class Bot:
         page.click("input[name=\"cnpj\"]")
         page.click("button:has-text(\"Consultar\")")
 
-        
-        # html = page.locator("#principal")
-        # html.text_content()
+        html = page.inner_html('#principal')
+        print(html)
 
+             
     with sync_playwright() as playwright:
         run(playwright)
