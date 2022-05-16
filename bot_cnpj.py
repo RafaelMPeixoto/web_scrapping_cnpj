2cf9d4cbe9dc4211d713409fe61252c805af7c57
RafaelMPeixoto <rafael_deicide@hotmail.com>
2022-05-16 15:21:38 -0300

    commit completar dados tabela

--- bot_cnpj.py
+++ bot_cnpj.py
@@ -1,29 +1,28 @@
 from cgitb import text
 from gettext import find
 from posixpath import split
+from re import T
 from xml.dom.minidom import Document
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup, Tag
 import pandas as pd
 
-
 class Bot:
-    def run(playwright):
+    def logging(playwright):
         firefox = playwright.firefox
-        browser = firefox.launch(headless=False, slow_mo=50)
+        browser = firefox.launch(headless=False)
         page = browser.new_page()
         page.goto("https://servicos.receita.fazenda.gov.br/servicos/cnpjreva/Cnpjreva_Solicitacao.asp")
 
         page.click('#captchaSonoro')
 
-        page.fill("input[name=\"cnpj\"]", "02.290.482/0001-97")
+        page.fill("input[name=\"cnpj\"]", "10.931.607/0001-49")
         page.click("input[name=\"cnpj\"]")
         page.click("button:has-text(\"Consultar\")")
-        html = page.inner_html('#principal')
         
+        html = page.inner_html('#principal')
         soup = BeautifulSoup(html, 'html.parser')
         table = soup.get_text('b')
-
 
         table = table.replace(' ',' ').replace('\t','').replace('\xa0','').replace('\n','').replace('b',' ')
 
@@ -31,16 +30,19 @@ class Bot:
         data_abertura = table.split('DATA DE ABERTURA')[1][4:28].strip()
         nome_empresarial = table.split('NOME EMPRESARIAL')[1][4:55].strip()
         titulo_estabelacimento = table.split('TÍTULO DO ESTABELECIMENTO (NOME DE FANTASIA)')[1][4:69].strip()
+        logradrouro = table.split('LOGRADOURO')[1][4:93].strip()
+        numero = table.split('NÚMERO    ')[1][0:15].strip()  
+        cep = table.split('CEP')[1][4:23].strip()
+        bairro = table.split('BAIRRO/DISTRITO')[1][4:63].strip()
+        municipio = table.split('MUNICÍPIO')[1][4:63].strip()
+        uf = table.split('UF')[1][4:20].strip()
+        email = table.split('ENDEREÇO ELETRÔNICO')[1][4:128].strip()
+        fone = table.split('TELEFONE')[1][4:32].replace(' ','').split()
+
+
         
-
-
-        # numero_inscricao = table.split('NÚMERO DE INSCRIÇÃO') # split retorna uma lista
-        # numero_inscricao = numero_inscricao[1]
-        # numero_inscricao = numero_inscricao[4:22]
-
-
     with sync_playwright() as playwright:
-        run(playwright)
+        logging(playwright)
         
         
         
