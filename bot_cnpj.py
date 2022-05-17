aaa699d2620968ad589c1074f90ba90df85f11b0
RafaelMPeixoto <rafael_deicide@hotmail.com>
2022-05-17 17:08:09 -0300

    Commit prints

--- bot_cnpj.py
+++ bot_cnpj.py
@@ -33,7 +33,7 @@ class Bot:
         numero_inscricao = table.split('NÚMERO DE INSCRIÇÃO')[1][4:24].strip()
         data_abertura = table.split('DATA DE ABERTURA')[1][4:28].strip()
         nome_empresarial = table.split('NOME EMPRESARIAL')[1][4:55].strip()
-        titulo_estabelacimento = table.split('TÍTULO DO ESTABELECIMENTO (NOME DE FANTASIA)')[1][4:69].strip()
+        titulo_estabelecimento = table.split('TÍTULO DO ESTABELECIMENTO (NOME DE FANTASIA)')[1][4:69].strip()
         logradrouro = table.split('LOGRADOURO')[1][4:93].strip()
         numero = table.split('NÚMERO    ')[1][0:15].strip()  
         cep = table.split('CEP')[1][4:23].strip()
@@ -43,33 +43,49 @@ class Bot:
         email = table.split('ENDEREÇO ELETRÔNICO')[1][4:128].strip()
         fone = table.split('TELEFONE')[1][4:32].replace(' ','').strip()
 
-        dados1 = (
-                    [numero_inscricao], 
-                    [data_abertura],    
-                    [nome_empresarial], 
-                    [titulo_estabelacimento],
-                    [logradrouro],
-                    [numero],
-                    [cep],
-                    [bairro],
-                    [municipio],
-                    [uf],
-                    [email],
-                    [fone])
+        dados1 = ([numero_inscricao],
+                  [data_abertura],
+                  [nome_empresarial],
+                  [titulo_estabelecimento],
+                  [logradrouro],
+                  [numero],
+                  [cep],
+                  [bairro],
+                  [municipio],
+                  [uf],
+                  [email],
+                  [fone]),"".join(map(str, dados1))
         
-        page.locator("text=Consultar QSA").click()
+        print(f'Número de Inscrição: {fone}'
+            '\n'f'Data de abertura: {data_abertura}'
+            '\n'f'Nome da Empresa: {nome_empresarial}'
+            '\n'f'Título do Estabelecimento: {titulo_estabelecimento}'
+            '\n'f'Endereço: {logradrouro}'
+            '\n'f'Número: {numero}'
+            '\n'f'CEP: {cep}'
+            '\n'f'Bairro: {bairro}'
+            '\n'f'Municipio: {municipio}'
+            '\n'f'UF: {uf}'
+            '\n'f'E-mail: {email}'
+            '\n'f'Fone: {fone}')
 
-        html = page.inner_html('#principal')
-        soup = BeautifulSoup(html, 'html.parser')
-        table = soup.get_text('div')
-
-        table = table.replace('\ndiv','').replace('\t','').replace('\n','').replace('\xa0div','').replace('div','') 
         
-        cnpj = table.split('CNPJ')[1][3:25].strip()
-        nome_empresarial1 = table.split('NOME EMPRESARIAL')[1][3:157].strip()
-        capital_social = table.split('CAPITAL SOCIAL')[1][3:39].strip()
+        # page.locator("text=Consultar QSA").click()
 
+        # html = page.inner_html('#principal')
+        # soup = BeautifulSoup(html, 'html.parser')
+        # table = soup.get_text('div')
+
+        # table = table.replace('\ndiv',' ').replace('\t',' ').replace('\n',' ').replace('\xa0div',' ').replace('div',' ') 
         
+        # cnpj = table.split('CNPJ')[1][3:25].strip()
+        # nome_empresarial1 = table.split('NOME EMPRESARIAL')[1][3:157].strip()
+        # capital_social = table.split('CAPITAL SOCIAL')[1][3:39].strip()
+
+        # nome_empresarial2 = table.split('Nome/Nome Empresarial')[1][1:151].strip()
+        # qualificacao = table.split('Qualificação')[1][1:23].strip()
+
+        # nome_empresarial3 = table.split('Nome Empresarial')   
 
     with sync_playwright() as playwright:
         run(playwright)
